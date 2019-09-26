1a013a21e8987767cc58f44aff7c689debd64003
catherine-o <catherine.ohara@outlook.com>
2019-09-26 11:06:33 -0600

    challenge complete

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,43 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import BotSpecs from '../components/BotSpecs'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    bots: [],
+    army: [],
+    selectedBot: ''
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(response => response.json())
+      .then(result => this.setState({ bots: result }))
+  }
+
+  updateSelected = (bot) => {
+    this.setState({ selectedBot: bot })
+  }
+
+  updateArmy = (bot) => {
+    this.setState({
+      army: [...this.state.army, bot]
+    }), this.renderCollection()
+  }
+
+  renderCollection = () => {
+    this.setState({ selectedBot: '' })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy army={this.state.army} />
+        {(this.state.selectedBot === '')
+          ? <BotCollection bots={this.state.bots} updateSelected={this.updateSelected} army={this.state.army} /> 
+          : <BotSpecs bot={this.state.selectedBot} updateArmy={this.updateArmy} renderCollection={this.renderCollection} /> 
+        }
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,15 +2,19 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+
+  createCard = (bots) => {
+    return bots.map(bot => {
+      return <BotCard bot={bot} army={this.props.army} />
+    })
+  }
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.createCard(this.props.army)}
           </div>
         </div>
       </div>
